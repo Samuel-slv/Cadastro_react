6ea16825ed31f5ac0271194d41ff57766d590954
Samuel da Silva <samuelsilva2017ramos@gmail.com>
2025-04-09 08:31:44 -0300

    news files

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import './style.css'
+
+function Home() {
+  
+  return (
+    <div>
+      <from action="">
+        
+        <h1>cadastro de usuario</h1>
+        <input type="text" name="name" id="" placeholder="entre com seu nome: "/>
+        <input type="text" name="email" id="" placeholder="entre com seu email: "/>
+        <input type="number" name="age" id=""/>
+        <button type=''>cadastrar </button>
+
+
+        </from> 
+        <div>
+          <div>
+            <p>Nome:</p>
+            <p>Email:</p>
+            <p>Age</p>
+          </div>
+          <button></button>
+        </div>
+      
+    </div>
+   
+  )
+}
+
+export default Home
